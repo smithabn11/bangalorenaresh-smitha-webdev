177da55ec17130360ac241395e5400c0768980b6
smithabn11 <smithabn77@gmail.com>
2016-12-05 15:37:06 -0500

    removing junk code

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var app1 = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -8,10 +7,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
-
-app1.use(bodyParser.json());
-app1.use(bodyParser.urlencoded({extended: true}));
-app1.use(express.static(__dirname + '/public'));
 
 app.all('/*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -26,6 +21,5 @@ require("./project/app.js")(app);
 var port = process.env.PORT || 3000;
 
 app.listen(port);
-app1.listen(3001);
 
 
